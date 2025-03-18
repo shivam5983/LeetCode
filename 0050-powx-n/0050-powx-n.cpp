b81fca86ccf979c2cde5fda09090626d2426694b
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-03-18 12:42:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (15.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==0){
+            return 0;}
+        else if(x==1){
+            return 1;
+        }    
+        else{
+            return pow(x,n);
+        }
+
+        }
+        
+        
+    
+    
+};
