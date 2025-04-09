bb4fad54d49b91775360df06f025d30bfea2c3eb
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-04-09 15:31:16 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.63%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int s=0;
+        int e=n-1;
 
-        int start=0;
-        int end=arr.size()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
 
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]==target){
+            if(nums[mid]==target){
                 return mid;
             }
-            else if(arr[start]<=arr[mid]){
-                if(arr[start]<=target && arr[mid]>=target){
-                    end=mid-1;
+            if(nums[mid]>=nums[s]){  // Left part sorted
+                if(nums[s]<=target && target<=nums[mid]){
+                    e=mid-1;
                 }
-            else {
-                start = mid + 1;
+                else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[e]){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
         }
-    }
-    else{
-        if(arr[mid]<=target && arr[end]>=target){
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
-        }
-    }
-        } 
         return -1;
-    
-    }  
+    }
 };
