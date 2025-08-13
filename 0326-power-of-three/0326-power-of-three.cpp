a8b8412f6bf51ddb9c7d4464c3e20225745e8a53
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-08-14 02:05:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n%3==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
+
+
+// C++
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if (n <= 0) return false;
+//         while (n % 3 == 0) {
+//             n /= 3;
+//         }
+//         return n == 1;
+//     }
+// };
