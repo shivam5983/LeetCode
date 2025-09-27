1bfdca4cab210e052686f830779c66082d1cf8e2
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-09-27 14:07:44 +0530

    Time: 0 ms (100%), Space: 8 MB (38.11%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        int n = word.length();
+        if(n<3) return false;
+
+        int vowels = 0, consonants=0;
+        string vowelList= "aeiouAEIOU";
+
+        for(char c: word){
+            if(isalpha(c)){
+                if(vowelList.find(c) != string::npos){
+                    vowels++;
+                }
+                else{
+                    consonants++;
+                }
+            }
+            else if (!isdigit(c)) {
+                return false; // invalid character
+            }
+
+        }
+
+
+        return vowels >= 1 && consonants >= 1;
+    }
+};
