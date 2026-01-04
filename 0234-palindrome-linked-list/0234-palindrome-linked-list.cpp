e0e3a178d3784c11b8c0fa1fd10b04629fd50dc5
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2026-01-05 02:31:34 +0530

    Time: 2 ms (63.79%), Space: 118 MB (66.64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> listVals;
-        while (head) {
-            listVals.push_back(head->val);
-            head = head->next;
+        if (head == NULL || head->next == NULL)
+            return true;
+
+        // Step 1: Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next!= NULL && fast->next->next!= NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        
-        int left = 0, right = listVals.size() - 1;
-        while (left < right && listVals[left] == listVals[right]) {
-            left++;
-            right--;
+
+        // Step 2: Reverse second half
+        ListNode* secondHalf = reverse(slow->next);
+
+        // Step 3: Compare both halves
+        ListNode* firstHalf = head;
+        ListNode* temp = secondHalf;
+
+        while (temp != NULL) {
+            if (firstHalf->val != temp->val)
+                return false;
+
+            firstHalf = firstHalf->next;
+            temp = temp->next;
         }
-        return left >= right;
+
+        return true;
+    }
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
     }
 };
