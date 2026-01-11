a7629adacc29ca0e9336c401aaf7446778db1244
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2026-01-12 02:19:30 +0530

    Time: 0 ms (100%), Space: 28.4 MB (7.5%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+         int n= nums.size();
+
+        unordered_map<int,int>mpp;
+
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+
+            if(mpp[nums[i]] >n/2) return nums[i];
+        }
+        return -1;
+    }
+};
